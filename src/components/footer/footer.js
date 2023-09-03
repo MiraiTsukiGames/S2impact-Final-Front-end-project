97c4c00f05baf485250e3137916b178fe0adaa97
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-09-03 19:35:18 +0200

    up

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <div className="text-center">
+    <div className="text-center b-0">
       Copyright@2023 Samanta Mancini
     </div>
   )
