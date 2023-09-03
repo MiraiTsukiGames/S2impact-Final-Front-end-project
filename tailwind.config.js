c8169946e7dc56ff08bd6fbaaecd88d1637d3f32
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-09-03 18:01:54 +0200

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
-/** @type {import('tailwindcss').Config} */
- module.exports = {
+
+
+module.exports = {
   darkMode: 'class',
   content: [
     "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
-}
+};
 
