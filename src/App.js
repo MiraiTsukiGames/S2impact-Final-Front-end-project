6efed7b8bf7a5523c39b2f7e066f5203cf5e60eb
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-09-03 17:07:30 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React from "react";
+import { useThemeContext } from "./context/theme";
 import './App.css';
 
 function App() {
+  // Get the current theme from the context
+const { contextTheme, setContextTheme } = useThemeContext();
+
+const getGradientClasses = () => {
+  if (contextTheme === "light") {
+    return "bg-gradient-to-b from-rose-700 via-rose-500 to-amber-200";
+  } else {
+    return "bg-gradient-to-b from-slate-950 via-slate-800 to-cyan-800";
+  }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        
+    <div className={`${getGradientClasses()} text-white p-8 mx-auto`}>
+    <div className="flex items-center justify-center h-screen">
+      <header className="py-4">
+      <h1 className='text-3xl lg:text-4xl align-middle text-center'>Hello</h1>
+      <button onClick={() => setContextTheme(contextTheme === "light" ? "dark" : "light")}>
+          {contextTheme === "light" ? "Dark" : "Light"} Mode
+        </button>
       </header>
+      <main>
+        {/* Il contenuto principale della tua applicazione */}
+      </main>
+    </div>
     </div>
   );
 }
---
+++ src/context/theme.js
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from "react";
+
+// Create a new context for the theme
+export const ThemeContext = createContext();
+
+// ThemeContextProvider component to provide the theme context to its children
+export const ThemeContextProvider = ({ children }) => {
+  // State to track the current theme
+  const [contextTheme, setContextTheme] = useState("Light");
+
+  // Create an object with the values to be provided in the context
+  const values = { contextTheme, setContextTheme };
+
+  return (
+    // Provide the values to the context
+    <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
+  );
+};
+
+// Custom hook to easily access the theme context values
+export const useThemeContext = () => {
+  // Use the useContext hook to access the theme context
+  const context = useContext(ThemeContext);
+  return context;
+};
+
+
