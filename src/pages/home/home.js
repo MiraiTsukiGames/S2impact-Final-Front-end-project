98682872fe3a4c1abe767d9ad2de7bcbe207dd59
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-09-08 17:46:32 +0200

    up

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -10,7 +10,10 @@ const Home = () => {
         <meta name="description" content="warming global app" />
       </Helmet>
       <Title text={"Global warming"}/>
-      <p className="text-left p-4 text-lg">Welcome to the Global Warming Data Recording App. <br></br>Explore graphs and statistics to understand the impact of Global Warming over time. <br></br>Contribute to the collection of valuable information for scientific research and informed decision-making.<br></br> Join our community of conscious citizens and help preserve our planet for future generations. </p>
+      <p className="text-left p-4 text-lg">Welcome to the Global Warming Data Recording App. <br></br>
+      Explore graphs and statistics to understand the impact of Global Warming over time. <br></br>
+      Contribute to the collection of valuable information for scientific research and informed decision-making.<br></br>
+      Join our community of conscious citizens and help preserve our planet for future generations. </p>
     </section>
   )
 }
