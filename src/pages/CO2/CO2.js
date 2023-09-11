faf07145fd3b5b130f43d2fa664ae1573150d1b3
MiraiTsukiGames <you@example.com>
2023-09-11 20:14:16 +0200

    up

--- src/pages/CO2/CO2.js
+++ src/pages/CO2/CO2.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Title from '../../components/title/Title';
-import { Helmet } from 'react-helmet';
 import Co2chart from '../../components/chart/Co2chart';
 import Button from '../../components/button/Button';
 import { useNavigate } from 'react-router-dom';
