a3fcd23a98753767c4f65efcf0168a7b82e712fb
MiraiTsukiGames <you@example.com>
2023-09-15 21:14:09 +0200

    up

--- src/pages/CO2/CO2.js
+++ src/pages/CO2/CO2.js
@@ -4,17 +4,21 @@ import Button from "../../components/button/Button";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Hero from "../../components/hero/Hero";
-
+import style from "./CO2.module.css";
 const CO2 = () => {
   const navigate = useNavigate();
 
   const { t } = useTranslation();
 
   return (
-    <section>
+    <section className={style.container}>
+      <div className={style.heroContainer}>
       <Hero title={"CO2"} description={t("co2description")} />
+      </div>
       <Co2chart />
+      <div className={style.buttonContainer}>
       <Button text={"Home"} onClick={() => navigate("/")} />
+      </div>
     </section>
   );
 };
