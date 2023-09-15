0d1d064277f6e54049d7fb8d3c09778619370b88
MiraiTsukiGames <you@example.com>
2023-09-15 13:54:58 +0200

    up

--- src/pages/temperature/Temperature.js
+++ src/pages/temperature/Temperature.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TemperatureChart from "../../components/chart/Temperaturechart";
+import TemperatureChart from "../../components/chart/Temperaturechart/Temperaturechart";
 import Button from "../../components/button/Button";
 import { useNavigate } from "react-router-dom";
 import Hero from "../../components/hero/Hero";
