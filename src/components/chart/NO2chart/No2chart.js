547891b335837da71919408b9aa76b3943b35a58
MiraiTsukiGames <you@example.com>
2023-09-15 13:57:07 +0200

    up

--- src/components/chart/NO2chart/No2chart.js
+++ src/components/chart/NO2chart/No2chart.js
@@ -9,7 +9,7 @@ import {
   Tooltip,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import useFetchData from "../api/ClientAPI";
+import useFetchData from "../../api/ClientAPI";
 import style from './No2.module.css';
 
 ChartJS.register(
