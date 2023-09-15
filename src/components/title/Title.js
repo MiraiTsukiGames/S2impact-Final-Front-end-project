efa371b7482aebe1fa0a393f9d00b292fc5e1f52
MiraiTsukiGames <you@example.com>
2023-09-15 14:48:11 +0200

    up

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "./Title.module.css";
 
 // title function
 const Title = ({ text }) => {
   return (
-    <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold overflow-hidden">
+    <h1 className={style.title}>
       {text}
     </h1>
   );
