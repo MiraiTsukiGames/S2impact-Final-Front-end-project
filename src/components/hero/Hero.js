fc986ee3aeacb556cf0a8d481dc0549c895b6288
MiraiTsukiGames <you@example.com>
2023-09-15 14:14:19 +0200

    up

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Title from "../title/Title";
+import style from "./Hero.module.css"
 
 export default function Hero({ title, description }) {
   return (
     <article>
-      <div>
-        <div>
+      <div className={style.container}>
+        <div className={style.title}>
           <Title text={title} />
         </div>
-        <div>
+        <div className={style.description}>
           <p>{description}</p>
         </div>
       </div>
