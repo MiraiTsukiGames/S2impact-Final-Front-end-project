9d3b37cb5874f93ef54c9565e970022d560cf824
MiraiTsukiGames <you@example.com>
2023-09-24 19:40:13 +0200

    up

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,10 +7,10 @@ export default function footer({ t }) {
     <footer className={style.footerContainer}>
       <div className={style.footerColor}>
         <p className={style.paragraph}>{t("text")}</p>
-        <ul>
-          <li><a href="https://miraitsukigames.github.io">Github</a></li>
-          <li><a href=""></a>linkedin</li>
-          <li><a href=""></a>e-mail</li>
+        <ul className={style.socials}>
+          <li><a target="_blank" href="https://github.com/MiraiTsukiGames" rel="noopener noreferrer">Github</a></li>
+          <li><a target="_blank" href="https://www.linkedin.com/in/samantamancini/" rel="noopener noreferrer">linkedin</a></li>
+          <li><a target="_blank" href="mailto:s.mancini93@hotmail.it" rel="noopener noreferrer">e-mail</a></li>
         </ul>
         <p className={style.copyright}>Copyright&copy; 2023 Samanta Mancini</p>
       </div>
