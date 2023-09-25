126d67a529defe9f2a3400d6bd3de6665b260a70
MiraiTsukiGames <you@example.com>
2023-09-25 17:02:42 +0200

    up

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,35 @@
 import React from "react";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import style from "./Footer.module.css";
 
 export default function footer({ t }) {
 
   return (
     <footer className={style.footerContainer}>
-      <div className={style.footerColor}>
-        <p className={style.paragraph}>{t("text")}</p>
-        <ul className={style.socials}>
-          <li><a target="_blank" href="https://github.com/MiraiTsukiGames" rel="noopener noreferrer">Github</a></li>
-          <li><a target="_blank" href="https://www.linkedin.com/in/samantamancini/" rel="noopener noreferrer">linkedin</a></li>
-          <li><a target="_blank" href="mailto:s.mancini93@hotmail.it" rel="noopener noreferrer">e-mail</a></li>
-        </ul>
-        <p className={style.copyright}>Copyright&copy; 2023 Samanta Mancini</p>
-      </div>
-    </footer>
+    <hr />
+    <div className={style.footerContent}>
+      <p className={style.paragraph}>{t("text")}</p>
+      <ul className={style.socials}>
+        <li className={style.link}>
+          <a target="_blank" href="https://github.com/MiraiTsukiGames" rel="noreferrer">
+            <FaGithub />
+          </a>
+        </li>
+        <li className={style.link}>
+          <a target="_blank" href="https://www.linkedin.com/in/samantamancini/" rel="noreferrer">
+            <FaLinkedin />
+          </a>
+        </li>
+        <li className={style.link}>
+          <a target="_blank" href="mailto:s.mancini93@hotmail.it" rel="noreferrer">
+            <FaEnvelope />
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div className={style.footerBottom}>
+      <p className={style.copyRightText}>Copyright&copy; {new Date().getFullYear()} Samanta Mancini</p>
+    </div>
+  </footer>
   );
 }
--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Spinner from "../../assets/img/gif/loading.gif";
+import "./Loading.css";
 
 export default function Loading() {
   return (
     <div className="loading-container">
-      <img src={Spinner} className="loading" alt="loading" />
+      <img src={Spinner} className="loading" alt="gif" />
         <p>Loading...</p>
   </div>
   );
