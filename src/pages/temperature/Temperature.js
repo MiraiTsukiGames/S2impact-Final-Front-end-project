1971657d04dedd52c199622cb54a151933ed33b8
MiraiTsukiGames <you@example.com>
2023-09-25 19:00:19 +0200

    up

--- src/pages/temperature/Temperature.js
+++ src/pages/temperature/Temperature.js
@@ -13,17 +13,6 @@ const Temperature = ({ t }) => {
       <meta name="description" content="temperature data" />
       <meta name="keywords" content="temperature, temperature grafico, temperature chart" />
       <meta name="author" content="Samanta Mancini"/>
-      <meta name="og:locale" content="it_IT"/>
-      <meta name="og:type" content="website"/>
-      <meta name="og:title" content="Temperature data"/>
-      <meta name="og:description" content="temperature data"/>
-      <meta property="og:site_name" content="Climate Change Alert"/>
-      <meta name="og:image" content="https://miraitsukigames.github.io/S2impact-Final-Front-end-project/src/assets/img/climatelogo.png"/>
-      <meta name="twitter:card" content="summary"/>
-      <meta name="twitter:site" content="@Climate Change Alert"/>
-      <meta name="twitter:title" content="Temperature data"/>
-      <meta name="twitter:description" content="temperature data"/>
-      <meta name="twitter:image" content="https://miraitsukigames.github.io/S2impact-Final-Front-end-project/src/assets/img/climatelogo.png"/>
     </Helmet>
 
     <section className={style.container}>
