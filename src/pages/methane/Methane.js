58ca79496026d76e8b7b67791a011713b4980437
MiraiTsukiGames <you@example.com>
2023-09-25 18:55:38 +0200

    up

--- src/pages/methane/Methane.js
+++ src/pages/methane/Methane.js
@@ -13,17 +13,6 @@ const Methane = ({ t }) => {
       <meta name="description" content="methane data" />
       <meta name="keywords" content="methane chart, metano, metano grafico, methane" />
       <meta name="author" content="Samanta Mancini"/>
-      <meta name="og:locale" content="it_IT"/>
-      <meta name="og:type" content="website"/>
-      <meta name="og:title" content="Methane"/>
-      <meta name="og:description" content="methane data"/>
-      <meta property="og:site_name" content="Climate Change Alert"/>
-      <meta name="og:image" content="https://miraitsukigames.github.io/S2impact-Final-Front-end-project/src/assets/img/climatelogo.png"/>
-      <meta name="twitter:card" content="summary"/>
-      <meta name="twitter:site" content="@Climate Change Alert"/>
-      <meta name="twitter:title" content="Methane data"/>
-      <meta name="twitter:description" content="methane data"/>
-      <meta name="twitter:image" content="https://miraitsukigames.github.io/S2impact-Final-Front-end-project/src/assets/img/climatelogo.png"/>
     </Helmet>
     <section className={style.container}>
       <Hero title={t("methane")} description={t("methanedescription")} />
