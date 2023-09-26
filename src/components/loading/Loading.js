8cf39cdde5f523e8d141b5be7f01f64b707c7c37
MiraiTsukiGames <you@example.com>
2023-09-26 20:17:17 +0200

    up

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -5,6 +5,7 @@ import "./Loading.css";
 export default function Loading() {
   return (
     <div className="loading-container">
+    {/* Render loading */}
       <img src={Spinner} className="loading" alt="gif" />
         <p>Loading...</p>
   </div>
