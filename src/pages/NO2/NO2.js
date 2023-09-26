2194c22c754e2e95809da02e645813a8d142f9d8
MiraiTsukiGames <you@example.com>
2023-09-26 10:48:38 +0200

    up

--- src/pages/NO2/NO2.js
+++ src/pages/NO2/NO2.js
@@ -8,7 +8,9 @@ const NO2 = ({ t }) => {
 
   return (
     <>
+    {/* Metadata and title tag */}
     <Helmet>
+    {/* Set the title for the page using translation */}
       <title>NO2 {t("title3")}</title>
       <meta name="description" content="nitrous data" />
       <meta name="keywords" content="no2, no2 grafico, no2 chart" />
@@ -16,8 +18,10 @@ const NO2 = ({ t }) => {
     </Helmet>
     <section className={style.container}>
     <div className={style.heroContainer}>
+    {/* Render the Hero component with translated title and description */}
       <Hero title={"NO2"} description={t("no2description")} />
       </div>
+      {/* Render the No2Chart component */}
       <No2Chart />
     </section>
     </>
