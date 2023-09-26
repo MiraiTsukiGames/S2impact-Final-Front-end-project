eb78cb6e4106800c089147836158b201ed135863
MiraiTsukiGames <you@example.com>
2023-09-26 09:32:47 +0200

    up

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,29 +1,37 @@
 import React, { useRef, useEffect, useState } from "react";
 import ButtonAnimation from "../../components/button/ButtonAnimation";
 import { Helmet } from "react-helmet-async";
-import style from "./Home.module.css";
 import Cards from "../../components/cards/Cards";
 import Hero from "../../components/hero/Hero";
+import style from "./Home.module.css";
+
 
 const Home = ({ t }) => {
+  // Ref to scroll into view
   const ref = useRef(null);
+  // State to track scroll position
   const [scrollTop, setScrollTop] = useState(0);
 
+  // Function to scroll up to the top
   const handleScrollUp = () => {
     ref.current?.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
+   // Function to scroll down to the end
   const handleScrollDown = () => {
     ref.current?.scrollIntoView({ behavior: "smooth", block: "end" });
   }
 
+  // Effect to update scroll position on scroll event
   useEffect(() => {
     const handleScroll = () => {
       setScrollTop(window.scrollY);
     };
 
+  // Add scroll event listener
     window.addEventListener("scroll", handleScroll);
-
+  
+  // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -31,6 +39,7 @@ const Home = ({ t }) => {
 
   return (
     <>
+    {/* Metadata and title tag */}
       <Helmet>
       <title>Climate Change Alert</title>
       <meta name="description" content="Climate Change Alert graphs" />
@@ -40,12 +49,19 @@ const Home = ({ t }) => {
     <section ref={ref}>
       <div className={style.banner}>
         <div className="home">
+        {/* Render the Hero component with title and description */}
           <Hero title={t("title")} description={t("description1")} />
         </div>
+        {/* Render the ButtonAnimation component for scrolling down */}
         <ButtonAnimation onClick={handleScrollDown} icon="arrowDown" />
       </div>
+
+      {/* Render the Cards component with scroll prop */}
       <Cards scroll={ref} />
+
+      {/* Show scroll up button when scrollTop is greater than 800 */}
       <div className={`${scrollTop > 800 ? style.show : style.hide}`}>
+      {/* Render the ButtonAnimation component for scrolling up */}
        <ButtonAnimation onClick={handleScrollUp} icon="arrowUp" />
       </div>
     </section>
