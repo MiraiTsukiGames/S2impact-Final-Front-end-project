49b46e7c613f146c8eca6681cd485b14b8b00cc6
MiraiTsukiGames <you@example.com>
2023-09-26 08:50:08 +0200

    up

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -46,7 +46,7 @@ const Home = ({ t }) => {
         <ButtonAnimation onClick={handleScrollDown} icon="arrowDown" />
       </div>
       <Cards scroll={ref} />
-      <div className={`${scrollTop > 0 ? "style.show" : "style.hide"}`}>
+      <div className={`${scrollTop > 0 ? style.show : style.hide}`}>
        <ButtonAnimation onClick={handleScrollUp} icon="arrowUp" />
       </div>
     </section>
